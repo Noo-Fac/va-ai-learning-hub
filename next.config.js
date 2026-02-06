3a7d03686f09d4c233e4cf07a284a6d31395d30e
Noosphere Factotum <noof@noospherefactotum.com>
2026-02-07 00:13:02 +0800

    Fix next.config.js - remove invalid serverActions config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  // Disable Server Actions to prevent build/runtime mismatch errors
-  experimental: {
-    serverActions: false,
-  },
 }
 
 module.exports = nextConfig
