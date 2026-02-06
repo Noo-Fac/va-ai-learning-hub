8ad785dd943a93f3fad7efcf49d2bf6eedf0959f
Noosphere Factotum <noof@noospherefactotum.com>
2026-02-06 23:47:18 +0800

    Disable Server Actions to fix build error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  // Disable Server Actions to prevent build/runtime mismatch errors
+  experimental: {
+    serverActions: false,
+  },
 }
 
 module.exports = nextConfig
